502024ecd77565b1822fe7c7825f4a188da6371d
sridr8 <saisuhas@saimail.com>
2019-08-26 20:12:14 +0530

    Initial Commit

---
+++ lib/main2.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Timer',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.deepOrange.shade400,
+          title: Text('Productivity Timer'),
+        ),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(5.0),
+              ),
+              Container(
+                height: 40.0,
+                width: 200.0,
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Work',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                        color: Colors.white),
+                  ),
+                  color: Colors.deepOrange,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(5.0),
+              ),
+              Container(
+                height: 40.0,
+                width: 200.0,
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Short Break',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                        color: Colors.white),
+                  ),
+                  color: Colors.lightGreen.shade400,
+                ),
+              ),
+              Padding(padding: EdgeInsets.all(5.0)),
+              Container(
+                height: 40.0,
+                width: 200.0,
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Long Break',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                        color: Colors.white),
+                  ),
+                  color: Colors.green.shade600,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(5.0),
+              ),
+              Center(
+                child: Container(
+                    child: CircleAvatar(
+                      child: Text('25:00',
+                          style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 35,
+                              color: Colors.grey)),
+                      backgroundColor: Colors.white,
+                      radius: 80.0,
+                    ),
+                    decoration: new BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: new Border.all(
+                        color: Colors.green.shade700,
+                        width: 10.0,
+                      ),
+                    )),
+              ),
+              Padding(padding: EdgeInsets.all(5.0)),
+              Container(
+                height: 40.0,
+                width: 200.0,
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Stop',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                        color: Colors.white),
+                  ),
+                  color: Colors.deepOrange,
+                ),
+              ),
+              Padding(padding: EdgeInsets.all(5.0)),
+              Container(
+                height: 40.0,
+                width: 200.0,
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Restart',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                        color: Colors.white),
+                  ),
+                  color: Colors.green.shade600,
+                ),
+              ),
+              Padding(padding: EdgeInsets.all(2.0)),
+              Expanded(child: Padding(padding: EdgeInsets.all(10.0)))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
